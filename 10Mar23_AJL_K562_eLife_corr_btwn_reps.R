2acf512e663cbc134222676cde4d199ff9bee014
Amanda Lea <amandalea7180@gmail.com>
2023-03-22 09:15:23 -0500

    Update 10Mar23_AJL_K562_eLife_corr_btwn_reps.R

--- 10Mar23_AJL_K562_eLife_corr_btwn_reps.R
+++ 10Mar23_AJL_K562_eLife_corr_btwn_reps.R
@@ -33,3 +33,52 @@ enh<-subset(res,info.y=='MD')
 smoothScatter(enh$norm_RNA_unmeth,enh$norm_RNA_meth)
 x=c(0,1);y=c(0,1);abline(lm(y~x),lty=2)
 
+##############
+# enhancers only
+##############
+
+res=fread('7Dec22_results_AJL_K562_eLife.txt')
+enh<-subset(res,info.x=='regulatory')
+
+# correlations between samples
+melted_cormat <- melt(cor(data[enh$id,]))
+melted_cormat2 <- merge(melted_cormat,info,by.x='Var1',by.y='sample')
+melted_cormat3 <- merge(melted_cormat2,info,by.x='Var2',by.y='sample')
+
+rna<-subset(melted_cormat3,type.x=='RNA' & type.y=='RNA' & value<1)
+
+# ggplot(data = rna, aes(x=Var1, y=Var2, fill=value)) + 
+#  geom_tile()+theme_bw(13)+scale_fill_gradient(low = "yellow", high = "blue", na.value = NA)
+
+rna$info<-NA
+rna$info[which(rna$treatment.x=='UN' & rna$treatment.y=='UN')]<-'UN_vs_UN'
+rna$info[which(rna$treatment.x=='M' & rna$treatment.y=='M')]<-'M_vs_M'
+
+rna<-subset(rna,info!='NA')
+ggplot(data = rna, aes(x=info, y=value)) + 
+  geom_boxplot()+theme_bw(13)+ylab('Pearson correlation')+xlab('Comparison')+ggtitle('Regulatory regions')
+
+##############
+# MD enhancers only
+##############
+
+res=fread('7Dec22_results_AJL_K562_eLife.txt')
+enh<-subset(res,info.y=='MD')
+
+# correlations between samples
+melted_cormat <- melt(cor(data[enh$id,]))
+melted_cormat2 <- merge(melted_cormat,info,by.x='Var1',by.y='sample')
+melted_cormat3 <- merge(melted_cormat2,info,by.x='Var2',by.y='sample')
+
+rna<-subset(melted_cormat3,type.x=='RNA' & type.y=='RNA' & value<1)
+
+# ggplot(data = rna, aes(x=Var1, y=Var2, fill=value)) + 
+#  geom_tile()+theme_bw(13)+scale_fill_gradient(low = "yellow", high = "blue", na.value = NA)
+
+rna$info<-NA
+rna$info[which(rna$treatment.x=='UN' & rna$treatment.y=='UN')]<-'UN_vs_UN'
+rna$info[which(rna$treatment.x=='M' & rna$treatment.y=='M')]<-'M_vs_M'
+
+rna<-subset(rna,info!='NA')
+ggplot(data = rna, aes(x=info, y=value)) + 
+  geom_boxplot()+theme_bw(13)+ylab('Pearson correlation')+xlab('Comparison')+ggtitle('MD regulatory regions')
